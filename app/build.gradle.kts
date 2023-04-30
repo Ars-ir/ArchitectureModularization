3c46154d8216e0f7531857c37f51da4893930344
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-04-30 17:58:51 +0700

    Fixing GraphQL package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("kotlin-android")
+    id("com.apollographql.apollo3").version("3.7.3")
+    kotlin("kapt")
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.architecturemodularization")
+    }
 }
 
 android {
@@ -39,11 +47,24 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // GraphQL
+    implementation("com.apollographql.apollo3:apollo-runtime:3.7.3")
+
+    // Coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
+    // LifeCycle
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+
+    // Koin
+    implementation ("io.insert-koin:koin-android:3.1.5")
 }
